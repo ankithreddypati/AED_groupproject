5578652d265f0249ae74ff8215cbd4bbb941d8d5
SruthiSivasankar <sivasankar.s@northeastern.edu>
2023-03-25 22:37:54 -0400

    certifier

--- src/Certifier/Certifier.java
+++ src/Certifier/Certifier.java
@@ -10,6 +10,31 @@ package Certifier;
  */
 public class Certifier {
     
+    private String name;
+    private int id;
+
+    public Certifier(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
     
     
 }
